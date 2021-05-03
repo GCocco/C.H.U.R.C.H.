0393f2a8f257c05428702e52e55216a046c355e7
GCocco <giammarco.cocco@gmail.com>
2021-05-03 16:29:10 +0200

    base panda3d class

---
+++ main.py
@@ -0,0 +1,13 @@
+from direct.showbase import ShowBase
+
+
+class Base(ShowBase):
+    def __init__(self):
+        super().__init__()
+        pass
+    pass
+
+
+if __name__ == "__main__":
+    Base().run()
+    pass
